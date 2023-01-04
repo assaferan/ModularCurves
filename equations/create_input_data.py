7d8dd743cbd5882fa2a9865ccb16b2b6407096bc
assaferan <assaferan@gmail.com>
2023-01-04 17:43:36 -0500

    fixed bug in finding stabilizers of orbits

--- equations/create_input_data.py
+++ equations/create_input_data.py
@@ -6,7 +6,7 @@ import sys
 # os.system("ulimit -n 4096")
 from lmfdb import db
 label = sys.argv[1]
-gens = db.gps_gl2zhat_test.lucky({'label' : label}, 'generators')
+gens = db.gps_gl2zhat_fine.lucky({'label' : label}, 'generators')
 os.system("mkdir -p input_data")
 genstr = ', '.join([str(g)[1:-1] for g in gens])
 f = open("input_data/" + label, 'w')
